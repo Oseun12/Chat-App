a0cd15f014062700ef605b4bbc3e0053ff763a9f
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-08-08 20:50:04 +0100

    Updated socket.io

--- api/server.js
+++ api/server.js
@@ -1,55 +1,12 @@
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-// const cors = require('cors');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server, {
-//   cors: {
-//     origin: ["https://chat-app-kappa-swart.vercel.app"], 
-//     methods: ["GET", "POST"],
-//     allowedHeaders: ["Content-Type"],
-//     credentials: true,
-//   },
-// });
-
-// app.use(cors({
-//   origin: ["https://chat-app-kappa-swart.vercel.app"], 
-//   methods: ["GET", "POST"],
-//   allowedHeaders: ["Content-Type"],
-//   credentials: true,
-// }));
-
-
-// io.on('connection', (socket) => {
-//   console.log('New client connected');
-  
-//   socket.on('sendMessage', (message) => {
-//     io.emit('receiveMessage', message);
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('Client disconnected');
-//   });
-// });
-
-
-// // module.exports = (req, res) => {
-// //   server(req, res);
-// // };
-// server.listen(4000, () => {
-//     console.log("Socket.io server running on http://localhost:4000");
-//   });
-
-// server.js
+// const express = require("express");
 const { Server } = require("socket.io");
 const { createServer } = require("http");
 
+// const app = express();
 const server = createServer();
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
@@ -70,4 +27,16 @@ io.on("connection", (socket) => {
 
 server.listen(4000, () => {
   console.log("Socket.io server running on http://localhost:4000");
+});
+
+// Handle server errors
+server.on("error", (err) => {
+  console.error("Server error:", err);
+});
+
+// Graceful shutdown
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
 });
