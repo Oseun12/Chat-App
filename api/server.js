a49d7c3aa66560f86fa66cf3d490b613fd099dae
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-08-08 20:20:23 +0100

    Updated socket.io

--- api/server.js
+++ api/server.js
@@ -49,7 +49,7 @@ const { createServer } = require("http");
 const server = createServer();
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
