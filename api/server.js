dd7e62f6db6e9d2909d3d33eb69c6181661bb192
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-08-09 14:27:58 +0100

    Socket Config

--- api/server.js
+++ api/server.js
@@ -8,9 +8,10 @@ const io = new Server(server, {
   cors: {
     origin: [
       "https://chat-ai1v8q0ih-marys-projects-cf8a8ef9.vercel.app",
-      "https://chat-app-two-black.vercel.app",
-      "https://chat-app-git-master-marys-projects-cf8a8ef9.vercel.app",
       "https://chat-4d6bfrukk-marys-projects-cf8a8ef9.vercel.app",
+      "https://chat-app-two-black.vercel.app",
+      "https://chat-g5c2bbgo3-marys-projects-cf8a8ef9.vercel.app", // Add your URL
+      "https://chat-a72mb5ic8-marys-projects-cf8a8ef9.vercel.app", // Add this URL if needed
       "http://localhost:3000",
     ],
     methods: ["GET", "POST"],
